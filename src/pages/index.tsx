8e29e56c6e45797996e1716530831f81d08e3b6e
Fabio da Silva Celeste <fabio.celeste@softwillians.com.br>
2022-07-09 21:32:46 -0300

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { GlobalStyle } from "../styles/global";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default Home;
