23137930e466a036c0a752b87f7979b189997fa3
Fabio da Silva Celeste <fabio.celeste@softwillians.com.br>
2022-07-11 11:18:54 -0300

    create home page

---
+++ src/components/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import Header from "../Header";
+import HomeItems from "../HomeItems";
+import { Container } from "./styles";
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Header />
+      <HomeItems />
+    </Container>
+  );
+};
+export default HomePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,12 @@
-import type { NextPage } from "next";
-
+import HomePage from "../components/HomePage";
 import { GlobalStyle } from "../styles/global";
-
-import Header from "../components/Header";
-import { useEffect, useState } from "react";
-import { getProducts } from "../services/products";
-import CardItem, { CardItemProps } from "../components/CardItem";
-import HomeItems from "../components/HomeItems";
 
 const Home = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <>
       <GlobalStyle />
-      <Header />
-      <HomeItems />
-    </div>
+      <HomePage />
+    </>
   );
 };
 
